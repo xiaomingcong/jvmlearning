79fe7dd599a3fff1b2e487f3389efe3f14b75e6f
xiaomingcong <>
2019-11-02 23:42:16 +0800

    commit suanfa project

---
+++ algorithm.learning/src/test/java/com/xmc/learning/algorithm/learning/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmc.learning.algorithm.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
